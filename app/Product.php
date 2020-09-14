1b83a119c1829e5bbe0c833a14baede9e66506d4
engineererick <engineererick@hotmail.com>
2020-09-14 09:59:54 -0500

    capitulo 58

--- app/Product.php
+++ app/Product.php
@@ -4,6 +4,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use App\Cart;
 use App\Order;
+use App\Image;
 
 class Product extends Model {
 	/**
@@ -21,5 +22,9 @@ class Product extends Model {
 	
 	public function orders(){
         return $this->belongsToMany(Order::class)->withPivot('quantity');
-    }
+	}
+	
+	public function images(){
+		return $this->morphMany(Image::class, 'imageable');
+	}
 }
